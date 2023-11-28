d3af4ce03c8f1c15d4aad43407fe5441bd4f4548
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2023-11-28 11:15:25 +0600

    Updating Ordinary Card

--- src/layouts/reception/data/dataOrdinary.js
+++ src/layouts/reception/data/dataOrdinary.js
@@ -1,124 +1,85 @@
 /* eslint-disable react/prop-types */
+import React from "react";
 import VuiTypography from "components/VuiTypography/index";
-
-// totalsList page components
 import IdCell from "layouts/clients/components/IdCell";
 import DefaultCell from "layouts/reception/components/DefaultCell";
 import StatusCell from "layouts/clients/components/StatusCell";
 import CustomerCell from "layouts/reception/components/CustomerCell";
+import VuiButton from "components/VuiButton";
+import { IoDesktop, IoCard } from "react-icons/io5";
 
 // Images
 import team1 from "assets/images/team-1.jpg";
 import team2 from "assets/images/team-2.jpg";
 import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
-import VuiButton from "components/VuiButton";
-
-import { Link } from "react-router-dom";
-import { IoDesktop } from "react-icons/io5";
-import { IoCard } from "react-icons/io5";
-
-
 
 export default {
-  
   columns: [
-    // { Header: "ID", accessor: "id", 
-    
-    //   Cell: ({ value }) => <IdCell id={value} /> 
-    
-    // },
-    { Header: "NAME", accessor: "name",
-
-    Cell: ({ value: [name, data] }) => (
-      <CustomerCell image={data.image} color={data.color || "dark"} name={name} />
-    ),
-  },
-    { Header: "DATE", accessor: "date", Cell: ({ value }) => <DefaultCell value={value} />},
-    // { Header: "STATUS", accessor: "status",
-
-    //   Cell: ({ value }) => {
-    //     let status;
-
-    //     if (value === "paid") {
-    //       status = <StatusCell icon="done" color="success" status="Paid" />;
-    //     } else if (value === "refunded") {
-    //       status = <StatusCell icon="replay" color="dark" status="Refunded" />;
-    //     } else {
-    //       status = <StatusCell icon="close" color="error" status="Canceled" />;
-    //     }
-
-    //     return status;
-    //   },
-    // },
-   
-    // { Header: "TOTAL", accessor: "total",
-    
-    // Cell: ({ value }) => {
-    //   const numericValue = parseInt(value);
-    
-    //   return (
-    //     <DefaultCell
-    //       value={numericValue}
-    //     />
-    //   );
-    // },
-    
-    
-    // },
-    { Header: "ACTION", accessor: "action", Cell: ({ value }) => <DefaultCell value={value} /> },
+    { Header: "Name", accessor: "name",
+      Cell: ({ value: [name, data] }) => (
+        <CustomerCell image={data.image} color={data.color || "dark"} name={name} />
+      ),
+    },
+    { Header: "Card Number", accessor: "cardNumber", Cell: ({ value }) => <DefaultCell value={value} /> },
+    { Header: "Type", accessor: "type", Cell: ({ value }) => <DefaultCell value={value} /> },
+    { Header: "Actions", accessor: "actions", Cell: ({ row }) => (
+        <div>
+          <VuiButton color="info" variant="contained" size="small" style={{ marginRight: '8px' }} onClick={() => handleEdit(row)}>
+            <IoDesktop style={{ marginRight: '8px' }} /> Edit
+          </VuiButton>
+          <VuiButton color="error" variant="contained" size="small" style={{ marginRight: '8px' }} onClick={() => handleDelete(row)}>
+            <IoDesktop style={{ marginRight: '8px' }} /> Delete
+          </VuiButton>
+          <VuiButton color="warning" variant="contained" size="small" onClick={() => handleUnbind(row)}>
+            <IoDesktop style={{ marginRight: '8px' }} /> Unbind
+          </VuiButton>
+        </div>
+      ),
+    },
   ],
 
   rows: [
     {
-      id: "#10421",
-      date: "20 Feb, 2023",
-      status: "paid",
       name: ["Carson Neil", { image: team2 }],
-      total: "4",
-      action:  (
-        <Link to="">
-          <VuiButton color="primary" variant="contained" size="small"> <IoCard style={{ marginRight: '8px' }} /> Issue Card</VuiButton>
-        </Link>
-      ),
+      cardNumber: "1234 5678 9012 3456",
+      type: "Guest",
     },
     {
-      id: "#10422",
-      date: "10 Jan, 2023",
-      status: "paid",
       name: ["Sadyrov Maksat", { image: team1 }],
-      total: "5",
-      action:  (
-        <Link to="">
-          <VuiButton color="primary" variant="contained" size="small"> <IoCard style={{ marginRight: '8px' }} /> Issue Card</VuiButton>
-        </Link>
-      ),
+      cardNumber: "9876 5432 1098 7654",
+      type: "Guest",
     },
     {
-      id: "#10423",
-      date: "12 Nov, 2023",
-      status: "refunded",
       name: ["Ibraev Temirbek", { image: "M", color: "info" }],
-      total: "7",
-      action:  (
-        <Link to="">
-          <VuiButton color="primary" variant="contained" size="small"> <IoCard style={{ marginRight: '8px' }} /> Issue Card</VuiButton>
-        </Link>
-      ),
+      cardNumber: "1111 2222 3333 4444",
+      type: "Guest",
     },
     {
-      id: "#10424",
-      date: "1 Dec, 2023",
-      status: "paid",
       name: ["Kemirkulov Kurban", { image: team3 }],
-      total: "29",
-      action:  (
-        <Link to="">
-          <VuiButton color="primary" variant="contained" size="small"> <IoCard style={{ marginRight: '8px' }} /> Issue Card</VuiButton>
-        </Link>
-      ),
+      cardNumber: "5555 6666 7777 8888",
+      type: "Guest",
     },
-    
+    {
+      name: ["Abdul Malik", { image: team1 }],
+      cardNumber: "9999 8888 7777 6666",
+      type: "Guest",
+    },
   ],
 };
 
+// Define action handlers
+const handleEdit = (row) => {
+  console.log(`Edit action for row with ID ${row.id}`);
+
+};
+
+const handleDelete = (row) => {
+  console.log(`Delete action for row with ID ${row.id}`);
+ 
+};
+
+const handleUnbind = (row) => {
+  
+  console.log(`Unbind action for row with ID ${row.id}`);
+ 
+};
